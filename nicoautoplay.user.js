09af5eff432f821ce73bf22e7e3a0b9850c4e81c
Hiroaki Kataoka <k_hir@hotmail.com>
2014-08-31 20:26:05 +0900

    fix inserting script.

--- nicoautoplay.user.js
+++ nicoautoplay.user.js
@@ -4,18 +4,16 @@
 // @include        http://www.nicovideo.jp/watch/*
 // @version        1.0.0
 // @description    ニコニコ動画で動画を自動再生できるようにする
-// @grant          GM_log
+// @grant          none
 // ==/UserScript==
 
 (function(){
 
   if (document.getElementById('nicoplayerContainerInner')) {
-
     var script = document.createElement("script");
     script.type = 'text/javascript';
-    script.textContent = 'WatchApp.ns.init.PlayerInitializer.nicoPlayerConnector.playerAreaConnector.addEventListener("onVideoInitialized",function(){WatchApp.ns.model.player.NicoPlayerConnector.playVideo();});';
+    script.textContent = 'function f(){try{WatchApp.ns.init.PlayerInitializer.nicoPlayerConnector.playerAreaConnector.addEventListener("onVideoInitialized",function(){WatchApp.ns.model.player.NicoPlayerConnector.playVideo();});WatchApp.ns.model.player.NicoPlayerConnector.playVideo();}catch(e){setTimeout(f,1000);}}f();';
     document.body.appendChild(script);
-
   }
 
 })();
